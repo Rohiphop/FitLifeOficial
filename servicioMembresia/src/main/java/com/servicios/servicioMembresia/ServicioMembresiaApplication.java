a3c3e508e89fcc0b6235040f4bebc565e31f5151
Rohiphop <rodrigoj.jara.mendez@gmail.com>
2025-05-26 07:24:08 -0400

    membresia

---
+++ servicioMembresia/src/main/java/com/servicios/servicioMembresia/ServicioMembresiaApplication.java
@@ -0,0 +1,13 @@
+package com.servicios.servicioMembresia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioMembresiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioMembresiaApplication.class, args);
+	}
+
+}
