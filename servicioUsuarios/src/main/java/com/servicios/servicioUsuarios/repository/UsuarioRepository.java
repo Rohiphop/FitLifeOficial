a57b7f0c4c1f46afc2d537886ccf1eebe1588493
Rohiphop <rodrigoj.jara.mendez@gmail.com>
2025-05-26 03:59:39 -0400

    usuario

--- servicioUsuarios/src/main/java/com/servicios/servicioUsuarios/repository/UsuarioRepository.java
+++ servicioUsuarios/src/main/java/com/servicios/servicioUsuarios/repository/UsuarioRepository.java
@@ -1,5 +1,10 @@
 package com.servicios.servicioUsuarios.repository;
 
-public class UsuarioRepository {
 
+import com.fitlife.usuarios.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    Optional<Usuario> findByCorreo(String correo);
 }
